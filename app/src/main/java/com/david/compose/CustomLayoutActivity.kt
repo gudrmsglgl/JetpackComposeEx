1a773475dbf4c55a0b49d1480ef275083ac58958
gudrmsglgl <gudrmsglgl@gmail.com>
2021-09-01 23:50:31 +0900

    feat(layout): Modifier layout {}

---
+++ app/src/main/java/com/david/compose/CustomLayoutActivity.kt
@@ -0,0 +1,75 @@
+package com.david.compose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.AlignmentLine
+import androidx.compose.ui.layout.FirstBaseline
+import androidx.compose.ui.layout.Placeable
+import androidx.compose.ui.layout.layout
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.david.compose.ui.theme.ComposeExTheme
+
+class CustomLayoutActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeExTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview3() {
+    ComposeExTheme {
+        Text(text = "Hi there!", Modifier.firstBaselineToTop(32.dp))
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview4() {
+    ComposeExTheme {
+        Text(text = "Hi there!", Modifier.padding(32.dp))
+    }
+}
+
+
+fun Modifier.firstBaselineToTop(
+    firstBaselineToTop: Dp
+) = this.then(
+    layout { measurable, constraints ->
+        val placeable: Placeable = measurable.measure(constraints)
+
+        // Check the composable has a first baseline
+        check(placeable[FirstBaseline] != AlignmentLine.Unspecified)
+        val firstBaseline = placeable[FirstBaseline]
+
+        // Height of the composable with padding - first baseline
+        val placeableY = firstBaselineToTop.roundToPx() - firstBaseline
+        val height = placeable.height + placeableY
+        layout(placeable.width, height) {
+            // Where the composable gets placed
+            placeable.placeRelative(0, placeableY)
+        }
+    }
+)
--- app/src/main/java/com/david/compose/ListComposeActivity.kt
+++ app/src/main/java/com/david/compose/ListComposeActivity.kt
@@ -18,7 +18,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.AlignmentLine
+import androidx.compose.ui.layout.FirstBaseline
+import androidx.compose.ui.layout.Placeable
+import androidx.compose.ui.layout.layout
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberImagePainter
 import com.david.compose.ui.theme.ComposeExTheme
@@ -95,7 +100,7 @@ fun ImageListItem(index: Int) {
             contentDescription = "Android Logo",
             modifier = Modifier.size(50.dp)
         )
-        Spacer(Modifier.width(10.dp))
+        Spacer(Modifier.width(15.dp))
         Text("Item #$index", style = MaterialTheme.typography.subtitle1)
     }
 }
